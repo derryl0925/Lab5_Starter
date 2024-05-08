a6140371fa29c701a4c5f6e8a6b2fc299efd87ca
derryl0925 <116408020+derryl0925@users.noreply.github.com>
2024-05-07 23:03:38 -0700

    Update expose.js
    fixed to where image was not popping up and noise wasn't playing either. Didn't notice until later

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,12 +1,61 @@
 // expose.js
-
 window.addEventListener('DOMContentLoaded', init);
-  
+
 function init() {
-  const playButton = document.querySelector('button');
-  const jsConfetti = new JSConfetti(); // Creating an instance of JSConfetti
-  
-  playButton.addEventListener('click', function() {
-    jsConfetti.addConfetti(); // Using default confetti settings
+  const jsConfetti = new JSConfetti();
+  const hornSelect = document.getElementById("horn-select");
+  const image = document.querySelector("img");
+  const audio = document.querySelector("audio");
+  const playButton = document.querySelector("button");
+  const volumeIcon = document.querySelector("#volume-controls img");
+  const volumeSlider = document.getElementById("volume");
+
+  // Function to update the image and sound based on the horn selection
+  function updateHorn() {
+    const hornImages = {
+      "party-horn": "party-horn.svg",
+      "car-horn": "car-horn.svg",
+      "air-horn": "air-horn.svg"
+    };
+    const hornSounds = {
+      "party-horn": "party-horn.mp3",
+      "car-horn": "car-horn.mp3",
+      "air-horn": "air-horn.mp3"
+    };
+
+    const hornType = hornSelect.value;
+    image.src = hornImages[hornType] ? `assets/images/${hornImages[hornType]}` : "assets/images/no-image.png";
+    audio.src = hornSounds[hornType] ? `assets/audio/${hornSounds[hornType]}` : "";
+  }
+
+  // Function to adjust the volume icon based on slider position
+  function adjustVolumeIcon(volume) {
+    if (volume === 0) {
+      volumeIcon.src = "assets/icons/volume-level-0.svg";
+    } else if (volume < 0.33) {
+      volumeIcon.src = "assets/icons/volume-level-1.svg";
+    } else if (volume < 0.67) {
+      volumeIcon.src = "assets/icons/volume-level-2.svg";
+    } else {
+      volumeIcon.src = "assets/icons/volume-level-3.svg";
+    }
+  }
+
+  // Playing sound and trigger confetti on play button click
+  playButton.addEventListener("click", () => {
+    audio.play();
+    if (hornSelect.value === "party-horn") {
+      jsConfetti.addConfetti();
+    }
+  });
+
+  // Update horn sound and image when selection changes
+  hornSelect.addEventListener("change", updateHorn);
+
+  // Adjust the volume icon and audio volume when volume slider changes
+  volumeSlider.addEventListener("change", () => {
+    const volumeLevel = volumeSlider.value / 100;
+    audio.volume = volumeLevel;
+    adjustVolumeIcon(volumeLevel);
   });
 }
