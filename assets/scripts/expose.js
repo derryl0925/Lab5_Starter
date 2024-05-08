c7efec9548c9bee2f64ad4edd919ac562b839a3a
derryl0925 <116408020+derryl0925@users.noreply.github.com>
2024-05-07 22:24:48 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,12 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
-
+  
 function init() {
-  // TODO
+  const playButton = document.querySelector('button');
+  const jsConfetti = new JSConfetti(); // Creating an instance of JSConfetti
+  
+  playButton.addEventListener('click', function() {
+    jsConfetti.addConfetti(); // Using default confetti settings
+  });
 }
